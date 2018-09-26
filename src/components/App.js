5bd7eb4772d8b22d8278ac00dec4138918b4a52a
Vitor Cremonez <vitorcremonez@gmail.com>
2018-09-26 16:58:46 -0300

    Cleaning boiler plate create-react-app files

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import '../styles/App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                Nano Vanity
+            </div>
+        );
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './styles/index.css';
+import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
