6430e697a5992731b10268dd9c65d2cc1ebb0dd3
Vitor Cremonez <vitorcremonez@gmail.com>
2018-10-05 02:42:58 -0300

    Installing Reactstrap + Bootstrap

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
